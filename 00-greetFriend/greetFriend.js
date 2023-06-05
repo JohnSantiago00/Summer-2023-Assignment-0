99550e156bdfb26d4009a64c9df3b5cc67c29e18
John Santiago <santiago.john96@gmail.com>
2023-06-05 10:05:51 -0400

    Fix greetFriend function

--- 00-greetFriend/greetFriend.js
+++ 00-greetFriend/greetFriend.js
@@ -2,6 +2,6 @@ function greetFriend(name) {
   // Insert code here;
   return "Hello, " + name + "!";
 }
-console.log(greetFriend("Albert"));
+
 // Do not edit this line;
 module.exports = greetFriend;
