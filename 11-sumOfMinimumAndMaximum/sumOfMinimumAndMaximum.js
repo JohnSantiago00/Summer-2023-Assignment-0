e278691ef29174a93dd17f6b8312e81af8cd1e59
John Santiago <santiago.john96@gmail.com>
2023-06-06 09:28:13 -0400

    complete function

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,9 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+
+  let min = Math.min.apply(null, nums);
+  let max = Math.max.apply(null, nums);
+  return min + max;
 }
 
 // Do not edit this line;
