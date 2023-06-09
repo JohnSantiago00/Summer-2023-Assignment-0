9280b002be2055dfe1f6dac834a449968c6ec4e9
John Santiago <santiago.john96@gmail.com>
2023-06-09 16:08:20 -0400

    complete function

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,22 @@
 function pairSum(nums, target) {
   // Insert code here;
+
+  let set = new Set();
+
+  if (nums.length <= 1) {
+    throw new Error("ERROR: Need more numbers in the array");
+  }
+
+  for (let i in nums) {
+    let need = Math.abs(target - nums[i]);
+    if (set.has(nums[i])) {
+      return true;
+    } else {
+      set.add(need);
+    }
+  }
+
+  return false;
 }
 
 // Do not edit this line;
